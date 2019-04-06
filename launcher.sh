d2f9639873b36fa9fa2671dc0d013bd7e76b0822
Victor Jung <victor.jung@isen.yncrea.fr>
2019-04-06 11:23:06 +0200

    ajout argument stop pour script launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,13 @@
 
 #!/bin/bash
-{
-    python server.py
-}&
-if [[ $# -ne 1 ]]; then
-	chromium-browser --kiosk --app=http://127.0.0.1:4242/ --window-size=1366,768 --start-fullscreen --user-data-dir=/tmp
-elif [ $1 = "devmode" ] || [ $1 = "devMode" ]; then
+if [[ $1 = "stop" ]]; then
 	sleep 0.5
+	killall python server.py
+else
+	{
+    python server.py
+	}&
+fi
+if [ $# = 0 ]; then
+	chromium-browser --kiosk --app=http://127.0.0.1:4242/ --window-size=1366,768 --start-fullscreen --user-data-dir=/tmp
 fi
